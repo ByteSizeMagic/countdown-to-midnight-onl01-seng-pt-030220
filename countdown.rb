4d521f295898d2d2e519e9f574905474039153ed
Crystal Schooling <bytesizemagic@gmail.com>
2020-03-05 11:05:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,13 @@ def countdown(integer)
   end
   return "HAPPY NEW YEAR!"
 end
+
+ def countdown_with_sleep(integer)
+   count = integer
+   while count > 0
+     puts "#{count} SECOND(S)!"
+     count -= 1
+     sleep(1)
+   end
+   return "HAPPY NEW YEAR!"
+ end
